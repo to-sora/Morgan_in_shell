6d35534b2691413be8a86be884015c751ce38304
waito3209 <waitotsang@outlook.com>
2024-04-19 13:40:29 +0000

    fix install path error

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
-SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Create the absolute path to the run.sh script
 run_path="$SCRIPT_DIR/run.sh"
+echo $run_path
 chmod +x $run_path
 echo "source \"$run_path\" 1 1 false" >> ~/.bashrc
